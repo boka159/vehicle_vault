91d2641e79ce43c0022604be7b29c8fe79fc0f65
Boris <boris15993@gmail.com>
2023-11-19 22:58:22 +0100

    updating the params makes nextjs refetch the data

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+	const { manufacturer, year, model, limit, fuel } = filters;
+
 	const headers = {
 		"X-RapidAPI-Key": "1ba2488afdmsh18cfdc9716e75b2p133582jsn8485eeaae28a",
 		"X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
 	};
 
 	const response = await fetch(
-		"https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
+		`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
 		{ headers: headers }
 	);
 	const result = await response.json();
